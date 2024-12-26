16b2b18ea6bfbfc225d77f21a4bcc0be4ae37e9d
Jas <azoulayjason@gmail.com>
2024-12-26 20:37:04 +0100

    add fontawesome

--- src/app/components/yield-form/yield-form.component.ts
+++ src/app/components/yield-form/yield-form.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-yield-form',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './yield-form.component.html',
   styleUrl: './yield-form.component.scss'
 })
 export class YieldFormComponent {
 
+  public faCoffee = faCoffee;
 }
