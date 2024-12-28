a5810f3f09329658f3f22c60f413427ea8fb1563
Jas <azoulayjason@gmail.com>
2024-12-28 22:20:55 +0100

    fix background

--- src/app/components/card-result/card-result.component.ts
+++ src/app/components/card-result/card-result.component.ts
@@ -3,12 +3,15 @@ import { YieldResult } from '../../models/yield.model';
 import { CommonModule } from '@angular/common';
 import { SharedService } from '../../services/shared/shared.service';
 import { Subject, takeUntil } from 'rxjs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowUp, faChartArea } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-card-result',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ],
   templateUrl: './card-result.component.html',
   styleUrl: './card-result.component.scss'
@@ -18,6 +21,8 @@ export class CardResultComponent implements OnInit, OnDestroy{
   public yieldResult!: YieldResult;
 
   private unsubscribe = new Subject<void>();
+
+  public faChartUp = faArrowUp;
 
   constructor(
     private sharedService: SharedService
